fceac5206441d8f5bd2175894e799572487a9249
Danny Chen <dannyisadog10@gmail.com>
2023-02-02 21:35:20 +0800

    feat: add a global loader in layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,25 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import FullPageLoader from "../FullPageLoader";
+import { useState } from "react";
 
 const Layout = (props) => {
   const { children } = props;
+  const [showLoader, setShowLoader] = useState(false);
   return (
     <div>
       <Navbar />
       <main>
         {children}
+        <button
+          onClick={() => {
+            setShowLoader(true)
+          }}
+        >
+          Show Loader
+        </button>
       </main>
-      <Footer />
+      <FullPageLoader show={showLoader} setShow={setShowLoader} />
     </div>
   );
 }
